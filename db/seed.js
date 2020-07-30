56b4ce0b8daefafccb72e9a05db429f80bb89fcd
Michael Hall <mhalla40@gmail.com>
2020-07-30 09:28:37 -0400

    seeded model

--- db/seed.js
+++ db/seed.js
@@ -3,28 +3,37 @@ const monstersJson = require("./monsters.json");
 const monsterDetail = require("./monstersDetail.json");
 const Types = require("../models/Types");
 
-const monsters = monsterDetail.map((item) => {
+const monsters = monstersJson.results.map((item) => {
   const monster = {};
-  (monster.index = item.index), (monster.name = item.name);
+  monster.index = item.index;
+  monster.name = item.name;
   monster.url = item.url;
+  return monster;
 });
 
-Monster.remove({});
-Monster.collection
-  .insert(monstersJson)
-  .then((monsters) => {
-    console.log(monstersJson);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Monster.remove({});
+// Monster.collection
+//   .insert(monstersJson)
+//   .then((monsters) => {
+//     console.log(monstersJson);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-Types.remove({});
-Types.collection
-  .insert(monsterDetail)
-  .then((details) => {
-    console.log(monsterDetail);
-  })
-  .catch((err) => {
-    console.log(err);
+// Types.remove({});
+// Types.collection
+//   .insert(monsterDetail)
+//   .then((details) => {
+//     console.log(monsterDetail);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+Monster.deleteMany({}).then(() => {
+  Monster.create(monsters).then((monsters) => {
+    console.log(monsters);
+    process.exit();
   });
+});
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(require("./routes"));
 
 app.listen(3000, () => console.log("listening on port 3000"));
