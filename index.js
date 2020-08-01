51a6165b58f5584b2c51c5f2a57af62597587c9d
Michael Hall <mhalla40@gmail.com>
2020-08-01 14:08:00 -0400

    changed cors

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = require("./routes/index");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const app = express();
 
 app.use(cors());
-
-const app = express();
 
 app.use(bodyParser.json());
 app.use(require("./routes"));
